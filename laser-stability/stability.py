f441564afb7d6e564436dc4792ea47904d1a3e45
Joel Frederico <joelfred@slac.stanford.edu>
2015-05-27 15:20:07 -0700

    Fixed hacks

--- laser-stability/stability.py
+++ laser-stability/stability.py
@@ -42,7 +42,7 @@ def run_analysis(save=False, check=False, debug=False, verbose=False, movie=Fals
         input('No file chosen, press enter to close...')
         return
     loadfile = loadfile[1:]
-    loadfile = 'nas/nas-li20-pm00/E217/2015/20150504/E217_16808/E217_16808.mat'
+    # loadfile = 'nas/nas-li20-pm00/E217/2015/20150504/E217_16808/E217_16808.mat'
     loadname = os.path.splitext(os.path.basename(loadfile))[0]
 
     reply = mtqt.ButtonMsg(title='Show full analysis?', buttons=['Yes', 'No'], maintext='Show individual images analyzed? (MUCH slower)')
@@ -76,9 +76,9 @@ def run_analysis(save=False, check=False, debug=False, verbose=False, movie=Fals
     # Load data
     # ======================================
     savefile = os.path.join(os.getcwd(), 'local.h5')
-    # data = E200.E200_load_data(loadfile)
-    f = h5.File(savefile, 'r', driver='core', backing_store=False)
-    data = E200.Data(read_file = f)
+    data = E200.E200_load_data(loadfile)
+    # f = h5.File(savefile, 'r', driver='core', backing_store=False)
+    # data = E200.Data(read_file = f)
     
     # ======================================
     # Cameras to process
